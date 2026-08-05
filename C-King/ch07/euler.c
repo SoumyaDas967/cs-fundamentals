2f461e8ed329bae54c9477e96eb1b81feba7deb0
SoumyaDas967 <soumyawarx@gmail.com>
2026-08-05 13:48:06 +0530

    Updated the program so that it calculates untill the current term is less than a term entered by the user

--- C-King/ch07/euler.c
+++ C-King/ch07/euler.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
 int main(void){
-	double fact=1;
-	double sum=1;
-	int n;
+	double fact=1.0;
+	double sum=1.0;
+	double epsilon;
+	int i=1;
 
-	printf("Enter a value of n: ");
-	scanf("%d", &n);
+	printf("Enter a value of epsilon: ");
+	scanf("%lf", &epsilon);
 
-	if (n<=0){
+	if (epsilon <= 0){
 		printf("Error!\n");
 		return 1;
 	}
-
-	for(int i=1;i<=n;i++){
-		fact=fact*i;
+	while(1.0/fact>=epsilon){
 		sum=sum+(1.0/fact);
+		i++;
+		fact=fact*i;
 	}
 
 	printf("The value of e is: %.15f\n", sum);
